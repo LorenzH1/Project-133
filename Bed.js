79229c4c8fb5175326fee942c3fb9987025a7f60
LorenzH1 <72896156+LorenzH1@users.noreply.github.com>
2021-06-04 20:23:33 -0400

    Add files via upload

---
+++ Bed.js
@@ -0,0 +1,47 @@
+img = "";
+status = "";
+object = [];
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocoSsd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+    status =  true;
+    objectDetector.detect(img, gotResult);
+}
+
+function preload(){
+    img= loadImage('bed.jpg');
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    
+        console.log(results);
+    
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(var i = 0; i < object.length; i++ ){
+            document.getElementById("status").innerHTML = "Status: 1 Object is in the image and cocoSsd model has Detected 1 object";
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%" + object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+
+        }
+    }}
+function back(){
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+function Bowl(){
+    window.location = "Bowl.html";
+}
+
+function Cube(){
+    window.location = "Cube.html";
+}
+
+function Mouse(){
+    window.location = "Mouse.html"
+}
+
+function Headphone(){
+    window.location = "Headphone.html";
+}
+
+function Bed(){
+    window.location = "Bed.html";
+}
+
